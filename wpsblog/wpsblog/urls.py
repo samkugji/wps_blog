89cb0ef2e5ef5e4e1952a75f93f86eb4adafbec1
samkugji <samkugji@gmail.com>
2016-05-24 17:43:54 +0900

    update urls.py, views.py for zigbang data

--- wpsblog/wpsblog/urls.py
+++ wpsblog/wpsblog/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from wpsblog.controller import home, room
+from wpsblog.views import home, room
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
