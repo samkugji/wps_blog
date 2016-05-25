254e567b93bd2625cafcdd794c7d3f009902af85
samkugji <samkugji@gmail.com>
2016-05-25 13:56:45 +0900

    update urls.py, views.py - add function news in watcha

--- wpsblog/wpsblog/urls.py
+++ wpsblog/wpsblog/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from wpsblog.views import home, room
+from wpsblog.views import home, room, news
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     
     url(r'^$', home),
     url(r'rooms/(?P<room_id>\d+)/$', room),
+	url(r'^news/$', news),
 
 ]
 
--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 from django.http.response import HttpResponse
 
@@ -16,3 +17,33 @@ def room(request, room_id):
         response.content,
         content_type="application/json",
     )
+
+def news(request):
+	search = request.GET.get("search")
+	
+	response = requests.get("https://watcha.net/home/news.json?page=1&per=50")
+	news_dict = json.loads(response.text)
+	news_list = news_dict.get("news")
+
+	if search:
+	    news_list = list(filter(
+		    lambda news: search in news.get('title'),
+			news_list,
+		))
+
+	content = "<h1>News</h1>"+\
+			   "<p>This is news page.</p>" +\
+			   "<p>{count} 개의 영화 뉴스 정보가 있습니다.</p>".format(count=len(news_list)) +\
+			   "".join([
+					   "<h2>{title}</h2><img src={image_src}><p>{content}</p>".format(
+				title=news.get('title'),
+				image_src=news.get('image'),
+				content=news.get('content'),
+			)
+		    for news
+			in news_list
+		])
+	
+	return HttpResponse(
+		content,
+	)
